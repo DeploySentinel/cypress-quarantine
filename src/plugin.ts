9fe1125943ad20b6951f152bf20940dfb416a9b4
warren <5959690+wrn14897@users.noreply.github.com>
2023-02-12 19:23:16 -0800

    feat: onSkip task

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,35 +1,86 @@
-import styles from 'ansi-styles';
+import crypto from 'crypto';
 
-const log = (message: string) => {
-  console.log(
-    `${styles.yellow.open}${message}${styles.yellow.close}`,
-  );
+import styles from 'ansi-styles';
+import { api, utils } from '@deploysentinel/debugger-core';
+
+type ExtraConfig = {
+  apiUrl: string;
+  meta: Record<string, unknown>; // extra static data attached to payload
 };
+
+const log = (message: string) =>
+  console.log(`${styles.yellow.open}${message}${styles.yellow.close}`);
+
+const error = (message: any) =>
+  console.log(`${styles.red.open}${message}${styles.red.close}`);
+
+export const fetchSkippedTestCases = async (
+  url: string,
+  payload: {
+    path: string;
+    titles: string[];
+    meta: Record<string, any>;
+  },
+  topLevelKey?: string,
+) => {
+  try {
+    const axiosInstance = api.buildAxiosInstance({
+      timeout: 60000,
+      maxContentLength: Infinity,
+      maxBodyLength: Infinity,
+    });
+    const resp: any = await axiosInstance.post(url, payload);
+    if (utils.validators.isObject(resp.data)) {
+      return topLevelKey ? resp.data[topLevelKey] : resp.data;
+    }
+    return null;
+  } catch (e) {
+    error(e);
+    return null;
+  }
+};
+
+export const sha1 = (value: string) =>
+  crypto.createHash('sha1').update(value).digest('hex');
+
+export const getTestId = (path: string, titles: string[]) =>
+  sha1(
+    JSON.stringify({
+      path,
+      titles,
+    }),
+  );
 
 export default (
   on: Cypress.PluginEvents,
   config: Cypress.PluginConfigOptions,
+  extraConfig: ExtraConfig,
 ): [Cypress.PluginEvents, Cypress.PluginConfigOptions] => {
   const cypressVersion = config['version'];
   const envs = config['env'];
 
-  log('Starting plugin...')
-
-  on('before:run', (runDetails: Cypress.BeforeRunDetails) => {
-    const { browser, group, parallel, system, tag, config } = runDetails;
-  });
+  log('Starting plugin...');
 
   on('task', {
-    'debug': (msg: string) => {
-      log(msg);
+    onSkip: async ({ path, titles }: { path: string; titles: string[] }) => {
+      log(`Getting test id for ${path} > ${titles.join('-')}`);
+      try {
+        const skippedTestCases = await fetchSkippedTestCases(
+          extraConfig.apiUrl,
+          {
+            path,
+            titles,
+            meta: {
+              cypressVersion,
+              ...extraConfig.meta,
+            },
+          },
+        );
+      } catch (e) {
+        error(e);
+      }
       return null;
     },
-  });
-
-  on('before:spec', (spec: Cypress.Spec) => {
-  });
-
-  on('after:spec', (spec: Cypress.Spec) => {
   });
 
   on(
@@ -38,8 +89,7 @@ export default (
       runResults:
         | CypressCommandLine.CypressRunResult
         | CypressCommandLine.CypressFailedRunResult,
-    ) => {
-    },
+    ) => {},
   );
 
   return [on, config];
--- src/support.ts
+++ src/support.ts
@@ -1,5 +1,39 @@
+export const getTestTitles = (
+  cypressTest: Mocha.Test | Mocha.Suite | undefined,
+  titles: string[],
+  currentDepth = 0,
+): void => {
+  if (!cypressTest?.title || currentDepth > 32) {
+    return;
+  }
+
+  if (Array.isArray(titles)) {
+    titles.unshift(cypressTest.title);
+  }
+
+  getTestTitles(
+    cypressTest.parent,
+    titles,
+    currentDepth + 1,
+  );
+};
+
 // Global vars
-const isOpenMode = Cypress.config('isInteractive');
-const isComponentTest = Boolean(Cypress.config('devServer' as any));
+// const isOpenMode = Cypress.config('isInteractive');
+// const isComponentTest = Boolean(Cypress.config('devServer' as any));
 const getFilePath = () => Cypress.spec.relative;
 
+beforeEach(function () {
+  const currentTest = this.currentTest;
+  if (currentTest) {
+    const titles: string[] = [];
+    getTestTitles(currentTest, titles);
+    cy.task('onSkip', { path: getFilePath(), titles }).then(shouldSkip => {
+      if (shouldSkip) {
+        const ctx = currentTest.ctx;
+        ctx?.skip();
+      }
+      return null;
+    });
+  }
+});
